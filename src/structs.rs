3db8fe855006f7988e3618cd0311d6408f2452a7
khanhtc1202 <khanhtc1202@gmail.com>
2019-07-20 19:21:57 +0900

    method of struct

--- src/structs.rs
+++ src/structs.rs
@@ -44,6 +44,22 @@ impl Person {
     }
 }
 
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.height > other.height && self.width > other.width
+    }
+}
+
 pub fn run() {
     let mut c = Color {
         red: 255,
@@ -71,4 +87,12 @@ pub fn run() {
     println!("Person: {:?}", t_p);
     // below object will become invalid
     // println!("Person: {:?}", p);
+
+    let rect1 = Rectangle { width: 30, height: 50 };
+    let rect2 = Rectangle { width: 10, height: 40 };
+    let rect3 = Rectangle { width: 60, height: 45 };
+
+    println!("The area of rectangle = {}", rect1.area());
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
