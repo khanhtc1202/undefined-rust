7cee6e972e0533d4ebd6c27e13bd5575a82e3e4c
khanhtc1202 <khanhtc1202@gmail.com>
2019-09-27 14:31:30 +0900

    refcell pointer

--- src/smart_pointer.rs
+++ src/smart_pointer.rs
@@ -1,3 +1,9 @@
+/// RECAP
+/// - Rc<T> enables multiple owners of the same data; Box<T> and RefCell<T> have single owners.
+/// - Box<T> allows immutable or mutable borrows checked at compile time; Rc<T> allows only immutable borrows checked at compile time;
+///   RefCell<T> allows immutable or mutable borrows checked at runtime.
+/// - Because RefCell<T> allows mutable borrows checked at runtime, you can mutate the value inside the RefCell<T> even when the RefCell<T> is immutable.
+
 /// Cons List is a data struct which represents a list, each element in that list contains one value and
 /// the list from next element ( recursive type )
 // implement cons list using smart pointer Box<T>
@@ -126,6 +132,7 @@ fn test_drop() {
 /// of your program for `reading only`. To enable multiple mutable references too value, use RefCell<T> instead
 
 mod rc_list {
+    /// NOTE: please note that Rc<T> just be used in `single-threaded scenarios`
 
     use std::rc::Rc;
 
@@ -152,5 +159,115 @@ mod rc_list {
             println!("c = {:?} | count after creating c = {}", c, Rc::strong_count(&a));
         }
         println!("a = {:?} | count after c goes out of scope = {}", a, Rc::strong_count(&a));
+    }
+}
+
+mod ref_cell {
+    /// With references and `Box<T>`, the borrowing rules’ invariants are enforced at compile time.
+    /// With `RefCell<T>`, these invariants are enforced at runtime. With references, if you break these rules,
+    /// you’ll get a compiler error. With `RefCell<T>`, if you break these rules, your program will panic and exit.
+
+    /// NOTE: please note that RefCell<T> just be used in `single-threaded scenarios` like Rc<T>
+    /// Rust compiler will give you a compile-time error if you try using it in a multi-threaded context.
+
+    /// NOTE: you can mutate the value inside the RefCell<T> even when the RefCell<T> is immutable.
+
+    trait Messenger {
+        fn send(&self, msg: &str);
+    }
+
+    struct LimitTracker<'a, T: Messenger> {
+        messenger: &'a T,
+        value: usize,
+        max: usize,
+    }
+
+    impl<'a, T> LimitTracker<'a, T>
+        where T: Messenger {
+        pub fn new(messenger: &T, max: usize) -> LimitTracker<T> {
+            LimitTracker {
+                messenger,
+                value: 0,
+                max,
+            }
+        }
+
+        pub fn set_value(&mut self, value: usize) {
+            self.value = value;
+
+            let percentage_of_max = self.value as f64 / self.max as f64;
+
+            if percentage_of_max >= 1.0 {
+                self.messenger.send("Error: You are over your quota!");
+            } else if percentage_of_max >= 0.9 {
+                self.messenger.send("Urgent warning: You've used up over 90% of your quota!");
+            } else if percentage_of_max >= 0.75 {
+                self.messenger.send("Warning: You've used up over 75% of your quota!");
+            }
+        }
+    }
+
+    use std::cell::RefCell;
+
+    struct MockMessenger {
+        // sent_messages: Vec<String>,
+        sent_messages: RefCell<Vec<String>>,
+    }
+
+    impl MockMessenger {
+        fn new() -> MockMessenger {
+            // MockMessenger { sent_messages: vec![] }
+            MockMessenger { sent_messages: RefCell::new(vec![]) }
+        }
+    }
+
+    impl Messenger for MockMessenger {
+        fn send(&self, message: &str) {
+            // self.sent_messages.push(String::from(message));
+            self.sent_messages.borrow_mut().push(String::from(message));
+        }
+    }
+
+    #[test]
+    fn it_sends_an_over_75_percent_warning_message() {
+        let mock_messenger = MockMessenger::new();
+        let mut limit_tracker = LimitTracker::new(&mock_messenger, 100);
+
+        limit_tracker.set_value(80);
+
+        // assert_eq!(mock_messenger.sent_messages.len(), 1);
+        assert_eq!(mock_messenger.sent_messages.borrow().len(), 1);
+    }
+}
+
+/// Combine Rc<T> (multiple owners that have immutable access to single data) and
+/// RefCell<T> (single owner that can get mutate access at any times) => you will get an
+/// `multiple owners and can mutate` a.k.a `Rc<RefCell<T>>`
+mod multi_mutate_list {
+    use std::rc::Rc;
+    use std::cell::RefCell;
+
+    #[derive(Debug)]
+    enum List {
+        Cons(Rc<RefCell<i32>>, Rc<List>),
+        Nil,
+    }
+
+    use self::List::{Cons, Nil};
+
+    #[test]
+    fn test_multiple_mutate_list() {
+        let value = Rc::new(RefCell::new(5));
+
+        let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+
+        let b = Cons(Rc::new(RefCell::new(6)), Rc::clone(&a));
+        let c = Cons(Rc::new(RefCell::new(10)), Rc::clone(&a));
+
+        *value.borrow_mut() += 10;
+
+        println!("a after = {:?}", a);
+        println!("b after = {:?}", b);
+        println!("c after = {:?}", c);
     }
 }
