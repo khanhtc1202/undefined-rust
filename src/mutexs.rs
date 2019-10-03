c82ff2559247904cc7839aa9f2673ed27ab3e366
khanhtc1202 <khanhtc1202@gmail.com>
2019-10-03 20:09:38 +0900

    arc

--- src/mutexs.rs
+++ src/mutexs.rs
@@ -2,9 +2,11 @@ use std::sync::Mutex;
 
 pub fn run() {
 //    simple();
-    another_simple();
+//    another_simple();
+    multi_thread_mutex();
 }
 
+#[allow(dead_code)]
 fn simple() {
     let m = Mutex::new(5);
 
@@ -25,6 +27,7 @@ fn simple() {
     println!("m = {:?}", m); // expect 6
 }
 
+#[allow(dead_code)]
 fn another_simple() {
     let m = Mutex::new(5);
     let mut num = m.lock().unwrap();
@@ -32,4 +35,27 @@ fn another_simple() {
     println!("m = {:?}", m); // expect Mutex { data: <locked> } since lock still be hold by num, not m
     drop(num);
     println!("m = {:?}", m); // expect 6 since drop(num) free the lock and it turn back to the main thread
+}
+
+use std::thread;
+use std::sync::Arc;
+
+fn multi_thread_mutex() {
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+            *num += 1;
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
 }
