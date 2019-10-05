b460f6a3b1defa37bb45cd43101c624dbbdd6205
khanhtc1202 <khanhtc1202@gmail.com>
2019-10-05 23:54:20 +0900

    run for GScreen

--- src/polymorphism.rs
+++ src/polymorphism.rs
@@ -1,9 +1,9 @@
-/// trait object declaration: dyn Trait
-/// polymorphism at run time (just like normal OOP)
 pub trait Draw {
     fn draw(&self);
 }
 
+/// trait object declaration: dyn Trait
+/// polymorphism at run time (just like normal OOP)
 pub struct Screen {
     pub components: Vec<Box<dyn Draw>>,
 }
@@ -31,6 +31,72 @@ impl<T> GScreen<T>
     }
 }
 
-pub fn run() {
+pub struct Button {
+    pub width: u32,
+    pub height: u32,
+    pub label: String,
+}
 
+impl Draw for Button {
+    fn draw(&self) {
+        println!("Drawing button to screen")
+    }
+}
+
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        println!("Drawing select box to screen")
+    }
+}
+
+pub fn run() {
+//    let screen = Screen {
+//        components: vec![
+//            Box::new(SelectBox {
+//                width: 75,
+//                height: 10,
+//                options: vec![
+//                    String::from("Yes"),
+//                    String::from("Maybe"),
+//                    String::from("No")
+//                ],
+//            }),
+//            Box::new(Button {
+//                width: 50,
+//                height: 10,
+//                label: String::from("OK"),
+//            }),
+//        ],
+//    }; // this one is allowed since both SelectBox and Button is trait Draw object
+//
+//    screen.run();
+
+    // otherwise
+    let g_screen = GScreen {
+        components: vec![
+            Box::new(SelectBox {
+                width: 75,
+                height: 10,
+                options: vec![
+                    String::from("Yes"),
+                    String::from("Maybe"),
+                    String::from("No")
+                ],
+            }),
+//            // adding Button in component list is not allowed since GScreen holds a vec! of type SelectBox (for T)
+//            Box::new(Button {
+//                width: 50,
+//                height: 10,
+//                label: String::from("OK"),
+//            }),
+        ],
+    };
+
+    g_screen.run();
 }
