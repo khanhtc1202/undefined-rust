097b57972057b355f8e14f18214b3b769f66efb5
khanhtc1202 <khanhtc1202@gmail.com>
2019-10-07 19:19:26 +0900

    derefer raw pointer

--- src/advance_unsafe.rs
+++ src/advance_unsafe.rs
@@ -14,6 +14,19 @@
 pub fn run() {
     let mut num = 5;
 
+    // immutable raw pointer
     let r1 = &num as *const i32;
+
+    // mutable raw pointer
     let r2 = &mut num as *mut i32;
+
+    // an immutable raw pointer point to a location on memory
+    let address = 0x012345usize;
+    let r = address as *const i32;
+
+    unsafe {
+        println!("r1 is: {}", *r1);
+        println!("r2 is: {}", *r2);
+//        println!("Location on memory: {}", *r);
+    }
 }
