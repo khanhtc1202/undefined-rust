c4bee8e054f702e3c78d8ca3a6ae2cd3a9a5caf0
khanhtc1202 <khanhtc1202@gmail.com>
2019-10-07 19:03:01 +0900

    raw pointer init

--- src/advance_unsafe.rs
+++ src/advance_unsafe.rs
@@ -4,6 +4,16 @@
 /// - Access or modify a mutable static variable
 /// - Implement an unsafe trait
 
-pub fn run() {
+/// Difference from references and smart pointers, raw pointers:
+///
+/// - Are allowed to ignore the borrowing rules by having both immutable and mutable pointers or multiple mutable pointers to the same location
+/// - Aren’t guaranteed to point to valid memory
+/// - Are allowed to be null
+/// - Don’t implement any automatic cleanup
 
+pub fn run() {
+    let mut num = 5;
+
+    let r1 = &num as *const i32;
+    let r2 = &mut num as *mut i32;
 }
