e954bc650b79641e508c3e7e4976d96406005180
khanhtc1202 <khanhtc1202@gmail.com>
2019-10-15 00:11:25 +0900

    return closure

--- src/advance_func.rs
+++ src/advance_func.rs
@@ -22,3 +22,11 @@ fn test_twice() {
 /// you can always pass a function pointer as an argument for a function that expects a closure.
 /// >> `It’s best to write functions using a generic type and one of the closure traits so your functions can accept either functions or closures.`
 
+/// Return closure
+
+/// Closure is trait and in most of case when we want to return some trait, we return concrete type which implement
+/// that trait. In case of closure, there is no concrete type which impl Fn FnMut or FnOnce for us to return
+/// Then remember about trait object! we use it to return closure :))
+fn returns_closure() -> Box<dyn Fn(i32) -> i32> {
+    Box::new(|x| x + 1)
+}
