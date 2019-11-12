f32eab722599cd94d832f6a03d75f218efd34090
khanhtc1202 <khanhtc1202@gmail.com>
2019-11-12 15:39:21 +0900

    get macro

--- src/advance_marco.rs
+++ src/advance_marco.rs
@@ -77,3 +77,52 @@ fn test_derive_macro() {
 }
 
 /// NOTE: https://doc.rust-lang.org/book/ch19-06-macros.html
+
+/// Get marco
+macro_rules! get {
+      ($t:ty) => {
+          {
+              let mut line: String = String::new();
+              std::io::stdin().read_line(&mut line).unwrap();
+              line.trim().parse::<$t>().unwrap()
+          }
+      };
+      ($($t:ty),*) => {
+          {
+              let mut line: String = String::new();
+              std::io::stdin().read_line(&mut line).unwrap();
+              let mut iter = line.split_whitespace();
+              (
+                  $(iter.next().unwrap().parse::<$t>().unwrap(),)*
+              )
+          }
+      };
+      ($t:ty; $n:expr) => {
+          (0..$n).map(|_|
+              get!($t)
+          ).collect::<Vec<_>>()
+      };
+      ($($t:ty),*; $n:expr) => {
+          (0..$n).map(|_|
+              get!($($t),*)
+          ).collect::<Vec<_>>()
+      };
+      ($t:ty ;;) => {
+          {
+              let mut line: String = String::new();
+              std::io::stdin().read_line(&mut line).unwrap();
+              line.split_whitespace()
+                  .map(|t| t.parse::<$t>().unwrap())
+                  .collect::<Vec<_>>()
+          }
+      };
+      ($t:ty ;; $n:expr) => {
+          (0..$n).map(|_| get!($t ;;)).collect::<Vec<_>>()
+      };
+}
+
+#[test]
+fn test_get_macro() {
+    let (a, b) = get!(usize, usize);
+    println!("{} {}", a, b);
+}
